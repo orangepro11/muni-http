e5d3699e2efe364005824887f40bd8379a3eb105
不爱喝橙子汁 <yangziyi2001@outlook.com>
2021-10-22 11:38:59 +0800

    放弃fly

--- src/HttpRequest.ts
+++ src/HttpRequest.ts
@@ -48,85 +48,156 @@ class HttpRequest {
     return HttpRequest.instances;
   }
 
-  
+  /**
+   * 获取请求的url地址
+   * @author 不爱喝橙子汁
+   */
   public getRequestUrl(): RequestInfo {
     return this.requestUrl;
   }
 
+  /**
+   * 获取作为选项部分的参数
+   * @author 不爱喝橙子汁
+   */
   public getRequestOptions(): HttpConfig | undefined {
     return this.requestOptions;
   }
 
-  public getRequestInfo() {
+  /**
+   * 获取请求的url+选项
+   * @author 不爱喝橙子汁
+   */
+  public getRequestInfo(): {
+    url: RequestInfo;
+    options: HttpConfig | undefined;
+  } {
     return {
       url: this.requestUrl,
       options: this.requestOptions,
     };
   }
 
-  public setHeaders(val: HttpHeader) {
+  /**
+   * 设置请求头
+   * @author 不爱喝橙子汁
+   */
+  public setHeaders(val: HttpHeader): HttpRequest {
     this.headers = val;
     return this;
   }
 
+  /**
+   * 获取请求头
+   * @author 不爱喝橙子汁
+   */
   public getHeaders(): HttpHeader {
     return this.headers;
   }
 
+  /**
+   * 设置content-type，例如HttpContentType.form / HttpContentType.json
+   * 目前版本只支持这两种方式
+   * @author 不爱喝橙子汁
+   */
   public setContentType(val: HttpContentType): HttpRequest {
     this.headers['Content-Type'] = val;
     return this;
   }
 
+  /**
+   * 获取content-type
+   * @author 不爱喝橙子汁
+   */
   public getContentType(): HttpContentType {
     return this.headers['Content-Type'] as HttpContentType;
   }
 
+  /**
+   * 设置以Bearer 为前缀的token
+   * @author 不爱喝橙子汁
+   */
   public setBearerToken(val: string): HttpRequest {
     const token = 'Bearer ' + val;
     this.headers.Authorization = token;
     return this;
   }
 
+  /**
+   * 设置通用的token
+   * @author 不爱喝橙子汁
+   */
   public setToken(val: string): HttpRequest {
     this.headers.Authorization = val;
     return this;
   }
 
+  /**
+   * 获取当前设置的token
+   * @author 不爱喝橙子汁
+   */
   public getToken(): string | undefined {
     return this.headers.Authorization;
   }
 
+  /**
+   * 设置请求的基础路径
+   * @author 不爱喝橙子汁
+   */
   public setBaseURL(val: string): HttpRequest {
     this.baseURL = val;
     return this;
   }
 
-  public getBaseURL() {
+  /**
+   * 获取请求的基础路径
+   * @author 不爱喝橙子汁
+   */
+  public getBaseURL(): string {
     return this.baseURL;
   }
 
+  /**
+   * 清空当前设置的content-type
+   * @author 不爱喝橙子汁
+   */
   public clearContentType(): HttpRequest {
     this.headers['Content-Type'] = '';
     return this;
   }
 
+  /**
+   * 清空当前设置的token
+   * @author 不爱喝橙子汁
+   */
   public clearToken(): HttpRequest {
     this.headers.Authorization = '';
     return this;
   }
 
+  /**
+   * 清空当前设置的基础路径
+   * @author 不爱喝橙子汁
+   */
   public clearBaseURL(): HttpRequest {
     this.baseURL = '';
     return this;
   }
 
+  /**
+   * 清空全部设置
+   * @author 不爱喝橙子汁
+   */
   public reset(): HttpRequest {
     this.baseURL = '';
     this.headers = {};
     return this;
   }
 
+  /**
+   * 组合生成fetch的选项参数
+   * @author 不爱喝橙子汁
+   */
   private generateConfig(options: HttpConfig = {}) {
     let config: HttpConfig = {
       headers: this.headers as HeadersInit,
@@ -165,11 +236,6 @@ class HttpRequest {
 
   public afterResponse(callback: AfterResponseCallBack | undefined) {
     this.ResponseCallback = callback;
-    return this;
-  }
-
-  public afterParseResponse(callback: AfterParseResponseCallBack) {
-    this.parseResponseCallback = callback;
     return this;
   }
 
